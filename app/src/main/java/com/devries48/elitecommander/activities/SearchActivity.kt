6d31b58efc713a0d0f29afaf898932b4025117e1
devries48 <r.vries@live.com>
2021-05-09 20:57:17 +0200

    Toolbar styling

---
+++ app/src/main/java/com/devries48/elitecommander/activities/SearchActivity.kt
@@ -0,0 +1,12 @@
+package com.devries48.elitecommander.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.devries48.elitecommander.R
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
+}
